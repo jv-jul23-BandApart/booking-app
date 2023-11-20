7a0fe1ae7c324cf5e88cc851ff82b6670ca138b7
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-11-20 13:36:48 +0200

    [API] Added model layer (Accomodation, Amenity, Booking, Payment, User)

---
+++ src/main/java/com/bookingapp/models/Accommodation.java
@@ -0,0 +1,65 @@
+package com.bookingapp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "accommodations")
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private Type type;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String size;
+
+    @ManyToMany
+    @JoinTable(
+            name = "accomodations_amenteties",
+            joinColumns = @JoinColumn(name = "categories_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Amenity> amenities;
+
+    @Column(name = "daily_rate", nullable = false)
+    private BigDecimal dailyRate;
+
+    @Column(nullable = false)
+    private Integer availability;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    private enum Type {
+        HOUSE,
+        APARTMENT,
+        CONDO,
+        VACATION_HOME
+    }
+}
