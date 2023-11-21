ebc996149809bef860c3bf5b2eca33b72af2e9db
Olena <yelenastankevych@gmail.com>
2023-11-21 14:06:21 +0200

    Added StripeSessionProvider

---
+++ src/main/java/com/bookingapp/stripe/StripeSessionProvider.java
@@ -0,0 +1,49 @@
+package com.bookingapp.stripe;
+
+import static com.stripe.param.checkout.SessionCreateParams.LineItem.PriceData.ProductData.builder;
+
+import com.bookingapp.model.Booking;
+import com.bookingapp.model.Payment;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StripeSessionProvider {
+    private static final String CURRENCY = "usd";
+    @Value("${stripe.secret.key}")
+    private String secretKey;
+    @Value("${local.domain}")
+    private String localDomain;
+
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+
+    public Session createSession(Payment payment, Booking booking) throws StripeException {
+        SessionCreateParams params = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setSuccessUrl(localDomain + "/success")
+                .setCancelUrl(localDomain + "/cancel")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency(CURRENCY)
+                                .setUnitAmountDecimal(payment.getAmountToPay())
+                                .setProductData(builder()
+                                        .setName("Payment")
+                                        .setDescription(booking.getAccommodationId().getType() + " booking")
+                                        .build())
+                                .build())
+                        .setQuantity(1L)
+                        .build())
+                .build();
+        return Session.create(params);
+    }
+}
