afea9405515f22e6fb24f922a7451d3c7c480fc5
Olena <yelenastankevych@gmail.com>
2023-11-23 17:53:18 +0200

    implemented schedule for checking expired stripe session

--- src/main/java/com/bookingapp/repository/PaymentRepository.java
+++ src/main/java/com/bookingapp/repository/PaymentRepository.java
@@ -1,11 +1,19 @@
 package com.bookingapp.repository;
 
 import com.bookingapp.model.Payment;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findBySessionId(String sessionId);
+
+    @Query(value = """
+            SELECT * FROM payments WHERE created_at <= DATE_SUB(NOW(), interval 1 day)
+            AND NOT status = 'EXPIRED'""",
+            nativeQuery = true)
+    List<Payment> findAllExpiredPayments();
 }
--- src/main/java/com/bookingapp/service/SchedulerService.java
+++ src/main/java/com/bookingapp/service/SchedulerService.java
@@ -1,11 +1,21 @@
 package com.bookingapp.service;
 
+import com.bookingapp.model.Payment;
 import com.bookingapp.repository.PaymentRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
     private final PaymentRepository repository;
+
+    @Scheduled(cron = "${cron.payments.interval}")
+    private void trackExpiredStripeSession() {
+        List<Payment> payments = repository.findAllExpiredPayments()
+                .stream().peek(p -> p.setStatus(Payment.Status.EXPIRED)).toList();
+        repository.saveAll(payments);
+    }
 }
