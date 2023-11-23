c0959fc5e83e15257a81daa70010f0aa13719b93
ystankevych <105653924+ystankevych@users.noreply.github.com>
2023-11-23 21:32:28 +0200

    implemented schedule for checking expired stripe session (#37)
    Co-authored-by: Nikita <141251067+pavlov-n@users.noreply.github.com>
    Co-authored-by: Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>

--- src/main/java/com/bookingapp/repository/PaymentRepository.java
+++ src/main/java/com/bookingapp/repository/PaymentRepository.java
@@ -2,8 +2,10 @@ package com.bookingapp.repository;
 
 import com.bookingapp.model.Payment;
 import com.bookingapp.model.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +13,10 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findBySessionId(String sessionId);
 
     boolean existsPaymentByBookingUserAndStatus(User bookingUser, Payment.Status status);
+
+    @Query(value = """
+            SELECT * FROM payments WHERE created_at <= DATE_SUB(NOW(), interval 1 day)
+            AND NOT status = 'EXPIRED'""",
+            nativeQuery = true)
+    List<Payment> findAllExpiredPayments();
 }
